1952902811cd420d21f99bddd9a3b2a1232db721
uknbr <ukn_linux@yahoo.com.br>
2019-05-14 22:59:12 -0300

    INI config, system VARs

--- firstCommit.py
+++ firstCommit.py
@@ -1,28 +1,64 @@
 #!/usr/bin/env python
 import platform
 import os
+import random
 from datetime import datetime
 from flask import Flask
 
 app = Flask(__name__)
 
+# Get OS vars
+def get_var(name):
+	try:
+		var = os.environ[name]
+	except:
+		var = ""
+	return var
+
+# Config file
+def get_config(param):
+	try:
+	    from configparser import ConfigParser
+	except ImportError:
+	    from ConfigParser import ConfigParser  # ver. < 3.0
+
+	config = ConfigParser()
+	config.read('config.ini')
+	return config.get('app', param)
+
 @app.route('/')
 def index():
-    message = "<h2>Hello, {0}!</h2>".format(os.environ['CUSTOM_MESSAGE'])
-    return message
+	env_who = get_var("APP_WHO")
+	if not env_who:
+		env_who = "Visitor"
+
+	env_color = get_var("APP_COLOR")
+	if not env_color:
+		env_color = "black"
+
+	index = "<h2>First Commit!</h2>"
+	index += "<font color=\"{}\">Hello, {}.</font>".format(env_color, env_who)
+	return index
 
 @app.route('/info')
 def info():
-
-	with open('date.conf') as f:
-		start_date = f.readline()
-
+	start_date = get_config("date")
 	date_begin = datetime.strptime(start_date, '%d/%m/%y')
 	date_end = datetime.today()
 	date_delta = date_end - date_begin
 	
-	information = "<p>Running from <b>{}</b></p><p>First commit was <b>{}</b> days ago</p>".format(str(platform.uname()[1]), date_delta.days)
+	information = "<p>Running from <b>{}</b></p>".format(str(platform.uname()[1]))
+	information += "<p>First commit was <b>{}</b> days ago</p>".format(date_delta.days)
 	return information
+
+@app.route('/message')
+def message():
+	file_message = get_config("file")
+	if os.path.isfile(file_message):
+		message = "<p>{}</p>".format((random.choice(list(open(file_message)))))
+	else:
+		message = "<p>This is default message.</p>"
+	return message
 
 if __name__ == '__main__':
 	app.run(use_reloader=True, host='0.0.0.0', port=1234, debug=True)
--- startup.sh
+++ startup.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-
 echo "Running Python:"
 ./firstCommit.py
